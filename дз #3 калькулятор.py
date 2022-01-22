a2e40697b732f90045a56b31b6123161335f88e2
Marko Medyk <markomedik4@gmail.com>
2022-01-23 01:56:00 +0200

    калькулятор

--- дз #3 калькулятор.py
+++ дз #3 калькулятор.py
@@ -1,37 +1,125 @@
-a = input('введіть перше число: ')
-if a.isdigit() is True:
-    print(f'ваше перше число: {a}')
-else:
-    exit('ви ввели не число, перезапустіть програму')
 
-b = input('введіть друге число: ')
-if b.isdigit() is True:
-    print(f'ваше друге число: {b}')
-else:
-    exit('ви ввели не число, перезапустіть програму')
 
-print('''
-список операцій(в дужках вказані альтернативні назви):
-+(додавання)
--(віднімання)
-*(множення)
-/(ділення)
-**(степінь)
-''')
-c = input('введіть операцію: ')
-if c == '+' or c == 'додавання':
-    print(f'{a} + {b} = {int(a) + int(b)}')
-elif c == '-' or c == 'віднімання':
-    print(f'{a} - {b} = {int(a) - int(b)}')
-elif c == '*' or c == 'множення':
-    print(f'{a} * {b} = {int(a) * int(b)}')
-elif b == '0' and c == '/' or b == '0' and c == 'ділення':
-    exit('на 0 ділити не можна, перезапустіть програму')
-elif c == '/' or c == 'ділення':
-    print(f'{a} / {b} = {int(a) / int(b)}')
-elif c == '**' or c == 'степінь':
-    print(f'{a} ** {b} = {int(a) ** int(b)}')
+while True:
+        YES = ['+', 'yes', 'YES', 'так', 'ТАК', 'Yes', 'YES!', 'Yes!', 'yes!', 'так!', 'Так!', 'ТАК!', 'Так', 'y', 'Y']
+        NO = ['-', 'no', 'NO', 'ні', 'НІ', 'No', 'NO!', 'No!', 'no!', 'ні!', 'Ні!', 'НІ!', 'Ні', 'n', 'N']
+        error0 = 0
+        operations = ['+', 'додавання', '-', 'віднімання', '*', '/', '**', 'множення', 'ділення', 'степінь']
+# ПЕРШЕ ЧИСЛО
 
-else:
-    print('на жаль, ми ще не можемо виконати вашу операцію')
+        while True:
+            a = input('Введіть перше число: ')
+            if a.isdigit() is True:
+                print(f'Ваше перше число: {a}')
+                break
+            else:
+                while True:
+                    print('\nНа жаль, ми не можемо розпізнати ваш варіант, введіть, будь ласка, ще раз')
+                    again0 = input(f'''\nСпробувати ще раз?\n\n(Можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+
+                    if again0 in NO:
+                        exit('Гаразд. Програма деактивована')
+                    elif again0 in YES:
+                        break
+                    else:
+                        continue
+
+# ДРУГЕ ЧИСЛО
+
+        while True:
+
+            b = input('Введіть друге число: ')
+            if b.isdigit() is True:
+                print(f'Ваше друге число: {b}')
+                break
+
+            else:
+                while True:
+                    print('На жаль, ми не можемо розпізнати ваш варіант, введіть, будь ласка, ще раз')
+                    again0 = input(f'''\nСпробувати ще раз?\n\n(Можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+
+                    if again0 in NO:
+                        exit('Гаразд. Програма деактивована')
+                    elif again0 in YES:
+                        break
+                    else:
+                        continue
+
+# ОПЕРАЦІЇ і ПОМИЛКА ДІЛЕННЯ НА 0
+        error0 = None
+        if error0 == None:
+            while True:
+
+                print('''\nСписок операцій(в дужках вказані альтернативні назви):\n+(додавання)\n-(віднімання)\n*(множення)\n/(ділення)\n**(степінь)\n                ''')
+                c = input('Введіть операцію: ')
+
+                if c == '+' or c == 'додавання':
+                    print(f'{a} + {b} = {int(a) + int(b)}')
+                    break
+
+                elif c == '-' or c == 'віднімання':
+                    print(f'{a} - {b} = {int(a) - int(b)}')
+                    break
+
+                elif c == '*' or c == 'множення':
+                    print(f'{a} * {b} = {int(a) * int(b)}')
+                    break
+
+                elif b == '0' and c == '/' or b == '0' and c == 'ділення':
+
+                    again0 = input(f'''\nна 0 ділити не можна. Ввести інше число?\n\n(можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+
+                    if again0 in NO:
+                        print('На жаль, программа не може вирішити цю задачу')
+                    elif again0 in YES:
+                        continue
+                    else:
+                        while True:
+                            print('на жаль ми не можемо розпізнати ваш варіант, введіть, будь ласка ще раз')
+                            again0 = input(f'''\nна 0 ділити не можна. Ввести іншу операцію?\n\n(можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+
+                            if again0 in NO:
+                                exit('Гаразд. Програма деактивована')
+                            elif again0 in YES:
+                                break
+                            else:
+                                continue
+                elif c == '/' or c == 'ділення':
+                    print(f'{a} / {b} = {int(a) / int(b)}')
+                    break
+                elif c == '**' or c == 'степінь':
+                    print(f'{a} ** {b} = {int(a) ** int(b)}')
+                    break
+                else:
+                    while True:
+                        print('На жаль, ми не можемо розпізнати ваш варіант, введіть, будь ласка, ще раз')
+                        again1 = input(f'''\nСпробувати ще раз?\n\n(Можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+
+                        if again1 in NO:
+                            exit('Гаразд. Програма деактивована')
+                        elif again1 in YES:
+                            break
+                        else:
+                            continue
+
+
+# ЩЕ РАЗ?
+
+        again = input(f"""\nЩе раз?\n\n(можливі варіанти відповідей:\n{YES}\n{NO})\n\n""")
+        if again in NO:
+            exit()
+        elif again in YES:
+            continue
+        else:
+            while True:
+                print('на жаль ми не можемо розпізнати ваш варіант, введіть, будь ласка ще раз')
+                again0 = input(f'''\nЩе раз?\n\n(можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+
+                if again0 in NO:
+                    exit('Гаразд. Програма деактивована')
+                elif again0 in YES:
+                    break
+                else:
+                    continue
+
 

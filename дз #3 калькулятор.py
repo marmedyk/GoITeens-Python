b52edce0e0442eb7ee212fb7a9657c202b2cae24
Marko Medyk <markomedik4@gmail.com>
2022-01-24 18:23:16 +0200

    калькулятор

--- дз #3 калькулятор.py
+++ дз #3 калькулятор.py
@@ -3,14 +3,13 @@
 while True:
         YES = ['+', 'yes', 'YES', 'так', 'ТАК', 'Yes', 'YES!', 'Yes!', 'yes!', 'так!', 'Так!', 'ТАК!', 'Так', 'y', 'Y']
         NO = ['-', 'no', 'NO', 'ні', 'НІ', 'No', 'NO!', 'No!', 'no!', 'ні!', 'Ні!', 'НІ!', 'Ні', 'n', 'N']
-        error0 = 0
         operations = ['+', 'додавання', '-', 'віднімання', '*', '/', '**', 'множення', 'ділення', 'степінь']
 # ПЕРШЕ ЧИСЛО
 
         while True:
-            a = input('Введіть перше число: ')
-            if a.isdigit() is True:
-                print(f'Ваше перше число: {a}')
+            first_number = input('Введіть перше число: ')
+            if first_number.isdigit() is True:
+                print(f'Ваше перше число: {first_number}')
                 break
             else:
                 while True:
@@ -28,9 +27,9 @@ while True:
 
         while True:
 
-            b = input('Введіть друге число: ')
-            if b.isdigit() is True:
-                print(f'Ваше друге число: {b}')
+            second_number = input('Введіть друге число: ')
+            if second_number.isdigit() is True:
+                print(f'Ваше друге число: {second_number}')
                 break
 
             else:
@@ -46,61 +45,80 @@ while True:
                         continue
 
 # ОПЕРАЦІЇ і ПОМИЛКА ДІЛЕННЯ НА 0
-        error0 = None
-        if error0 == None:
-            while True:
 
-                print('''\nСписок операцій(в дужках вказані альтернативні назви):\n+(додавання)\n-(віднімання)\n*(множення)\n/(ділення)\n**(степінь)\n                ''')
-                c = input('Введіть операцію: ')
+        while True:
 
-                if c == '+' or c == 'додавання':
-                    print(f'{a} + {b} = {int(a) + int(b)}')
-                    break
+            # while True:
+            #
+            #     second_number = input('Введіть друге число: ')
+            #     if second_number.isdigit() is True:
+            #         print(f'Ваше друге число: {second_number}')
+            #         break
+            #
+            #     else:
+            #         while True:
+            #             print('На жаль, ми не можемо розпізнати ваш варіант, введіть, будь ласка, ще раз')
+            #             again0 = input(f'''\nСпробувати ще раз?\n\n(Можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+            #
+            #             if again0 in NO:
+            #                 exit('Гаразд. Програма деактивована')
+            #             elif again0 in YES:
+            #                 break
+            #             else:
+            #                 continue
 
-                elif c == '-' or c == 'віднімання':
-                    print(f'{a} - {b} = {int(a) - int(b)}')
-                    break
 
-                elif c == '*' or c == 'множення':
-                    print(f'{a} * {b} = {int(a) * int(b)}')
-                    break
+            print('''\nСписок операцій(в дужках вказані альтернативні назви):\n+(додавання)\n-(віднімання)\n*(множення)\n/(ділення)\n**(степінь)\n                ''')
+            operation = input('Введіть операцію: ')
 
-                elif b == '0' and c == '/' or b == '0' and c == 'ділення':
+            if operation == '+' or operation == 'додавання':
+                print(f'{first_number} + {second_number} = {int(first_number) + int(second_number)}')
+                break
 
-                    again0 = input(f'''\nна 0 ділити не можна. Ввести інше число?\n\n(можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+            elif operation == '-' or operation == 'віднімання':
+                print(f'{first_number} - {second_number} = {int(first_number) - int(second_number)}')
+                break
 
-                    if again0 in NO:
-                        print('На жаль, программа не може вирішити цю задачу')
-                    elif again0 in YES:
-                        continue
-                    else:
-                        while True:
-                            print('на жаль ми не можемо розпізнати ваш варіант, введіть, будь ласка ще раз')
-                            again0 = input(f'''\nна 0 ділити не можна. Ввести іншу операцію?\n\n(можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+            elif operation == '*' or operation == 'множення':
+                print(f'{first_number} * {second_number} = {int(first_number) * int(second_number)}')
+                break
 
-                            if again0 in NO:
-                                exit('Гаразд. Програма деактивована')
-                            elif again0 in YES:
-                                break
-                            else:
-                                continue
-                elif c == '/' or c == 'ділення':
-                    print(f'{a} / {b} = {int(a) / int(b)}')
-                    break
-                elif c == '**' or c == 'степінь':
-                    print(f'{a} ** {b} = {int(a) ** int(b)}')
-                    break
+            elif  second_number== '0' and operation == '/' or second_number == '0' and operation == 'ділення':
+
+                again0 = input(f'''\nна 0 ділити не можна. Ввести іншу операцію?\n\n(можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+
+                if again0 in NO:
+                    print('Гаразд. Програма деактивована')
+                elif again0 in YES:
+                    continue
                 else:
                     while True:
-                        print('На жаль, ми не можемо розпізнати ваш варіант, введіть, будь ласка, ще раз')
-                        again1 = input(f'''\nСпробувати ще раз?\n\n(Можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+                        print('на жаль ми не можемо розпізнати ваш варіант, введіть, будь ласка ще раз')
+                        again0 = input(f'''\nна 0 ділити не можна. Ввести іншу операцію?\n\n(можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
 
-                        if again1 in NO:
+                        if again0 in NO:
                             exit('Гаразд. Програма деактивована')
-                        elif again1 in YES:
+                        elif again0 in YES:
                             break
                         else:
                             continue
+            elif operation == '/' or operation == 'ділення':
+                print(f'{first_number} / {second_number} = {int(first_number) / int(second_number)}')
+                break
+            elif operation == '**' or operation == 'степінь':
+                print(f'{first_number} ** {second_number} = {int(first_number) ** int(second_number)}')
+                break
+            else:
+                while True:
+                    print('На жаль, ми не можемо розпізнати ваш варіант, введіть, будь ласка, ще раз')
+                    again1 = input(f'''\nСпробувати ще раз?\n\n(Можливі варіанти відповідей:\n{YES}\n{NO})\n\n''')
+
+                    if again1 in NO:
+                        exit('Гаразд. Програма деактивована')
+                    elif again1 in YES:
+                        break
+                    else:
+                        continue
 
 
 # ЩЕ РАЗ?
